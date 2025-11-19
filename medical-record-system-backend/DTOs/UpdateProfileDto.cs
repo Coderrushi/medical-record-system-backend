bf87d7974e35fb0bdb1e00431b334be46373966c
Hrushikesh Zarekar <96732405+Coderrushi@users.noreply.github.com>
2025-11-19 12:10:15 +0530

    Deleted duplicate medical-record-system-backend directory

--- medical-record-system-backend/DTOs/UpdateProfileDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace medical_record_system_backend.DTOs
-{
-    public class UpdateProfileDto
-    {
-        public string FullName { get; set; }
-        public string Phone { get; set; }
-        public string Gender { get; set; }
-    }
-}
